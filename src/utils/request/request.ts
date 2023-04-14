5b02ca4f43ae8a46c0b0e4820118689166e3e429
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-04-14 16:59:24 -0300

    Feat: create type generic to axios get

---
+++ src/utils/request/request.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { RequestConfig, Response } from './request.types';
+
+export class Request {
+  constructor(private request = axios) {}
+
+  public get<T>(url: string, config: RequestConfig = {}): Promise<Response<T>> {
+    return this.request.get<T, Response<T>>(url, config);
+  }
+}
