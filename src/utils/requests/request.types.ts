46d2aef1a3beee5c28622f0c00c017408fdcdf21
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-04-14 17:40:34 -0300

    delete path

--- src/utils/requests/request.types.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
-
-export interface RequestConfig extends AxiosRequestConfig {}
-
-export interface Response<T = any> extends AxiosResponse<T> {}
