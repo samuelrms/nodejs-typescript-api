7798b8b52da0a0cf9fb73cde9b56977cf2a93310
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-17 19:41:54 -0300

    fix: checked connection to mongodb

--- src/databse.ts
+++ src/databse.ts
@@ -5,6 +5,11 @@ const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
   await mongooseConnect(dbConfig.get('mongoUrl'));
+  if (connection.readyState === 1) {
+    console.log('Connected to MongoDB');
+  } else {
+    console.log('Failed to connect to MongoDB');
+  }
 };
 
 export const close = (): Promise<void> => connection.close();
