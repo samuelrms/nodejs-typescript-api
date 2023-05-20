9735e85a820cf1993c41bd0b05bfce185b9b21d0
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-20 13:15:29 -0300

    feat: validation error

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -15,5 +15,20 @@ describe('Users functional test', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
+
+    it('should throw 400 when there is a validation error', async () => {
+      const newUser = {
+        email: 'samuel@gmail.com',
+        password: '1234',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        code: 400,
+        error: 'User validation field: name: Path `name` is required',
+      });
+    });
   });
 });
